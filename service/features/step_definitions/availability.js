e652a7301a2461fff200ac402577b6ba29c62d85
Oresztesz Margaritisz <v-omargaritisz@hotels.com>
2019-05-29 15:15:50 +0200

    Adding before step

--- service/features/step_definitions/availability.js
+++ service/features/step_definitions/availability.js
@@ -1,17 +1,44 @@
 "use strict"
 const request = require('request');
 const assert = require('assert');
-const { Given, When, Then } = require('cucumber');
+const { Before, Given, When, Then } = require('cucumber');
+const toxiproxyClient = require("toxiproxy-node-client");
+// TODO to configuration
 const SERVICE_URL = 'http://localhost:8080/getData';
+const TOXIPROXY_URL = 'http://192.168.99.100:8474';
 
-Given('MySQL is down', function () {
-  // TODO
+
+Before( async function ()  {
+  const toxiproxy = new toxiproxyClient.Toxiproxy(TOXIPROXY_URL);
+  const MySQLProxy = {
+    listen: "0.0.0.0:3306",
+    name: "mysql",
+    upstream: "mysql:6379"
+  };
+  const result = toxiproxy.createProxy(MySQLProxy)
+    .then(proxy => {
+      this.proxy = proxy;
+    })
+    .catch(Promise.reject);
+  await result;
+});
+
+Given('MySQL is down', async function () {
+  const toxic = new toxiproxyClient.Toxic(this.proxy, {
+    name: 'mysql_down',
+    stream: 'downstream',
+    toxicity: 1,
+    type: 'down'
+  });
+  const result = this.proxy.addToxic(toxic)
+    .then(console.dir)
+    .catch(Promise.reject);
+  await result;
 });
 
 
 When('user {string} is requested', function (string, callback) {
   // Write code here that turns the phrase above into concrete actions
-  console.log("Hello");
   request(SERVICE_URL, { json: true }, (err, res, body) => {
     if (err) {
       callback(err);
