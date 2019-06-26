97df0915a86bcc2a0515592ab0982f29d4b2b4f4
Oresztesz Margaritisz <v-omargaritisz@hotels.com>
2019-06-26 13:12:38 +0200

    Removing shell initialization. Compose is simpler.

--- add_proxies.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-/go/bin/toxiproxy-cli -h toxiproxy:8474 create mysql --listen 0.0.0.0:3306 --upstream mysql:3306 ||
-/go/bin/toxiproxy-cli -h toxiproxy:8474 create redis-master --listen 0.0.0.0:6379 --upstream redis-master:6379 ||
-/go/bin/toxiproxy-cli -h toxiproxy:8474 create redis-slave --listen 0.0.0.0:16379 --upstream redis-slave:6379
